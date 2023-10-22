0a1708a52c6f9761adc85a7d45d299446f258750
Guisos <guisos@UbuntuMV.myguest.virtualbox.org>
2023-10-22 13:34:09 -0300

    Añadido filtros

--- src/pages/proyectos.jsx
+++ src/pages/proyectos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import projectData from '../assets/data.json';
 import NavBar from '../components/nav_bar'
 import projectShow from '../components/projectShow';
@@ -49,12 +49,43 @@ export default function ProyectosPage(){
 
   const [projectList, setProjectList] = useState(dataList);
 
+  const [uniqueTags, setUniqueTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState("");
+
+  useEffect(() => {
+    const allTags = dataList.flatMap((project) => project.tags);
+    const uniqueTags = Array.from(new Set(allTags));
+    setUniqueTags(uniqueTags);
+  }, []);
+
+  const handleTagChange = (event) => {
+    setSelectedTags(event.target.value);
+    if (event.target.value === "") {
+      setProjectList(dataList);
+    } else {
+      const filteredProjects = dataList.filter((project) =>
+        project.tags.includes(event.target.value)
+      );
+      setProjectList(filteredProjects);
+    }
+  };
+
   return (
     <div className='page'>
       <NavBar />
       <h1 className='page__title'>Proyectos</h1>
+      <div>
       <h2>Buscar</h2>
-      {projectShow(dataList)}
+      <select value={selectedTags} onChange={handleTagChange}>
+        <option value="">Todos</option>
+        {uniqueTags.map((tag) => (
+          <option key={tag} value={tag}>
+            {tag}
+          </option>
+        ))}
+      </select>
+      </div>
+      {projectShow(projectList)}
     </div>
   )
 }
