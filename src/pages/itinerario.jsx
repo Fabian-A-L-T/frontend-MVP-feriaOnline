5b274274642b6528c576e9f53ea07d46c422e3c9
Benjamín Esteban Soto Alfán <105157002+FoxOfTheVoid@users.noreply.github.com>
2023-10-22 13:07:55 -0300

    Update itinerario.jsx

--- src/pages/itinerario.jsx
+++ src/pages/itinerario.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React from 'react';
+import NavBar from '../components/nav_bar';
+import '../stylesheets/ItinerarioPage.css'; // Importa el archivo SCSS
 
-import NavBar from '../components/nav_bar'
+const ItinerarioPage = () => {
+  const itinerario = [
+    { tipo: 'Charla', hora: '08:00 AM', link: 'https://ejemplo.com/Charla' , color: 'yellow'},
+    { tipo: 'Taller', hora: '10:00 AM', link: 'https://ejemplo.com/Taller' , color: 'red'},
+    { tipo: 'Livestream', hora: '01:00 PM', link: 'https://ejemplo.com/Live' , color: "lightblue"},
+    // Agrega más elementos según sea necesario
+  ];
 
-export const ItinerarioPage = () => {
+  // Función para generar la tabla JSX
+  const generarTabla = () => {
+    return (
+      <table className="centered-table">
+        <thead>
+          <tr>
+            <th>Tipo</th>
+            <th>Hora</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itinerario.map((item, index) => (
+            
+            <tr key={index} style={{ backgroundColor: item.color }}>
+              <td>{item.tipo}</td>
+              <td>{item.hora}</td>
+              <td>
+                <a href={item.link}>{item.link}</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
   return (
     <div className='page'>
       <NavBar />
       <h1 className='page__title'>Itinerario</h1>
+      {generarTabla()}
     </div>
-  )
-}
+  );
+};
 
-export default ItinerarioPage
+export default ItinerarioPage;
