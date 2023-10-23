a24606dd0ccd7a1602d72b89c6310d3ae58cbc79
Fabian Lorca <fabian.lorca22@gmail.com>
2023-10-22 22:14:35 -0300

    Datos realistas

--- src/pages/proyectos.jsx
+++ src/pages/proyectos.jsx
@@ -5,45 +5,7 @@ import projectShow from '../components/projectShow';
 
 export default function ProyectosPage(){
 
-  var data = {
-    "Projects":[
-        {
-            "softwareName": "algo",
-            "teamName": "test 1",
-            "tags": ["IA","IoT"],
-            "img": "/fesw-logo.png",
-            "favCounter": 12
-        },
-        {
-            "softwareName": "algo 1",
-            "teamName": "test 2",
-            "tags": ["Seguridad"],
-            "img": "/usm.png",
-            "favCounter": 8
-        },
-        {
-            "softwareName": "algo 2",
-            "teamName": "test 3",
-            "tags": ["Diseño", "3D"],
-            "img": "/fesw-logo.png",
-            "favCounter": 2
-        },
-        {
-            "softwareName": "algo 3",
-            "teamName": "test 4",
-            "tags": ["IA"],
-            "img": "/usm.png",
-            "favCounter": 19
-        },
-        {
-            "softwareName": "algo 4",
-            "teamName": "test 5",
-            "tags": ["IoT"],
-            "img": "/fesw-logo.png",
-            "favCounter": 26
-        }
-    ]
-}
+  var data = projectData
   var dataList = data.Projects
   dataList.sort((a, b) => (a.favCounter < b.favCounter) ? 1 : -1)
 
